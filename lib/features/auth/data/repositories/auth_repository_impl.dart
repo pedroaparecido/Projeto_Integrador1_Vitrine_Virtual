087d9a67700165931139359fd4bce339a6373cf5
Isaac Garcia <2101317@aluno.univesp.br>
2023-05-02 21:05:33 -0300

    Login (#11)
    * login crud
    
    * adicionado entidade, modelo e data_sources
    
    ---------
    
    Co-authored-by: Pedro Ap <2107281@aluno.univesp.br>

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,20 @@
+import '../../domain/repositories/auth_repository.dart';
+import '../data_sources/auth_data_source.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  AuthRepositoryImpl(this._dataSource);
+
+  final AuthDataSource _dataSource;
+
+  @override
+  Future<bool> signIn({required String email, required String password}) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
---
+++ lib/features/auth/domain/entities/auth_entity.dart
@@ -0,0 +1,14 @@
+import 'package:vitrine_virtual/features/company/domain/entities/company_entity.dart';
+
+class AuthEntity {
+  AuthEntity({
+    this.company,
+    this.isLogged = false,
+  });
+
+  CompanyEntity? company;
+  bool isLogged;
+
+  @override
+  String toString() => 'AuthEntity(company: $company, isLogged: $isLogged)';
+}
